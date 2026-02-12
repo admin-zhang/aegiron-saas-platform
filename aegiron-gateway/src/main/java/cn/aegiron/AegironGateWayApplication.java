f468ec4f20ce39839d09e7fea9b84336e3d622b5
admin-zhang <764018641@qq.com>
2026-02-12 16:12:49 +0800

    feat(gateway): 添加网关服务基础框架
    - 创建网关启动类 AegironGateWayApplication
    - 添加测试类 AegironGateWayApplicationTest
    - 配置 banner.txt 显示服务信息
    - 设置 bootstrap.yml 配置文件支持 nacos 注册中心和服务发现
    - 完成 pom.xml 依赖配置，包含 spring cloud gateway、nacos、负载均衡等组件
    - 集成 knife4j 接口文档功能
    - 添加 Apache Commons Lang 工具包依赖

---
+++ aegiron-gateway/src/main/java/cn/aegiron/AegironGateWayApplication.java
@@ -0,0 +1,18 @@
+package cn.aegiron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author panda
+ * @version 1.0
+ * @ClassName AegironGateWayApplication.java
+ * @Description 网关启动类
+ * @since 2026/2/12 15:58
+ */
+@SpringBootApplication
+public class AegironGateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AegironGateWayApplication.class, args);
+    }
+}
---
+++ aegiron-gateway/src/test/java/cn/aegiron/AegironGateWayApplicationTest.java
@@ -0,0 +1,14 @@
+package cn.aegiron;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author panda
+ * @version 1.0
+ * @ClassName AegironGateWayApplicationTest.java
+ * @Description 测试类
+ * @since 2026/2/12 16:09
+ */
+@SpringBootTest
+public class AegironGateWayApplicationTest {
+}

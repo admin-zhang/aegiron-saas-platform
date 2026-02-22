a98248a0b232c94feff574ef5a5c01dd33b4decf
admin-zhang <764018641@qq.com>
2026-02-22 21:10:59 +0800

    feat(auth): 新增认证服务模块
    - 创建了 AegironAuthApplication 主启动类
    - 添加了认证服务的测试类 AegironAuthApplicationTest
    - 配置了 banner.txt 启动横幅显示
    - 设置了 bootstrap.yml 微服务配置文件
    - 定义了 pom.xml 项目依赖和构建配置
    - 在父项目 pom.xml 中注册 auth 模块

---
+++ aegiron-auth/src/main/java/cn/aegiron/AegironAuthApplication.java
@@ -0,0 +1,19 @@
+package cn.aegiron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author panda
+ * @version 1.0
+ * @ClassName AegironAuthApplication.java
+ * @Description 认证服务启动类
+ * @since 2026/2/22 20:17
+ */
+@SpringBootApplication
+public class AegironAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AegironAuthApplication.class, args);
+    }
+}
---
+++ aegiron-auth/src/test/java/cn/aegiron/AegironAuthApplicationTest.java
@@ -0,0 +1,20 @@
+package cn.aegiron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author panda
+ * @version 1.0
+ * @ClassName AegironAuthApplication.java
+ * @Description
+ * @since 2026/2/22 20:17
+ */
+@SpringBootTest
+public class AegironAuthApplicationTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AegironAuthApplicationTest.class, args);
+    }
+}
